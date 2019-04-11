658c37e81b6fdef58f70e376e084a2b3edf3e8eb
xyy2006 <xyy62006@gmail.com>
2019-04-11 12:29:07 -0700

    update table schema to accomodate original factory downloaded csv format

--- librelink/postgreSQL_etl/raw_schema_etl.sql
+++ librelink/postgreSQL_etl/raw_schema_etl.sql
@@ -13,6 +13,7 @@ show search_path;
 SET search_path TO raw,public;
 
 /* the table stores user account information */
+drop table if exists raw.user_account;
 create table raw.user_account(
 	user_id serial PRIMARY KEY,
 	username VARCHAR (50) UNIQUE NOT NULL,
@@ -23,21 +24,32 @@ create table raw.user_account(
 );
 
 /* the table stores cgm_librelink (continuous glucose monitoring) information */
+drop table if exists raw.cgm_librelink;
 create table raw.cgm_librelink(
 	user_id serial PRIMARY KEY,
+	meter VARCHAR (50), -- meter brand/manufacturer
+	serial_number VARCHAR (355), -- hardware serial number
 	meter_timestamp TIMESTAMP NOT NULL,
 	record_type SMALLINT NOT NULL, -- (SMALLINT)  is 2-byte signed integer that has a range from -32,768 to 32,767.
 	historic_glucose SMALLINT, -- unit in mg/dL
 	scan_glucose SMALLINT, -- in mg/dL
+	non_numeric_rapid_acting_insulin boolean, -- indicator (flag)
+	rapid_acting_insulin_unit SMALLINT, -- unit in 'unit'
 	non_numeric_food boolean, --only 1 observed, as indicator (flag)
 	carbohydrates_gram SMALLINT, -- unit in grams
 	carbohydrates_serving SMALLINT, -- unit in servings
+	non_numeric_long_acting_insulin boolean, -- indicator (flag)
+	long_acting_insulin_unit SMALLINT, -- unit in 'unit'
 	notes  VARCHAR (355), -- verbal description of the food
 	strip_glucose SMALLINT, -- in mg/dL
-	ketone SMALLINT -- in mmol/L
-)
+	ketone SMALLINT, -- in mmol/L
+	meal_insulin SMALLINT, -- unit in 'unit'
+	correction_insulin SMALLINT, -- unit in 'unit'
+	user_change_insulin SMALLINT -- unit in 'unit'
+);
 
 /* the table stores whatever activities a user did such as eat, sleep, run, etc. */
+drop table if exists raw.activity_track;
 create table raw.activity_track(
 	user_id serial PRIMARY KEY,
 	start_time TIMESTAMP NOT NULL, -- activity start time
@@ -45,5 +57,5 @@ create table raw.activity_track(
 	activity_category VARCHAR(10) NOT NULL, -- activity category, e.g., food, sleep, exerise, event
 	comment VARCHAR(355) -- comment about food, etc.
 	-- duration numeric
-)
+);
 	
