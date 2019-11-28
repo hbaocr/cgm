5d652e0a7bf5116fbc7df19f38c0039a8aa20415
Richard Sprague <richardsprague@gmail.com>
2019-11-27 17:35:29 -0800

    uses library(cmgr) instead of local code.

--- librelink/server.R
+++ librelink/server.R
@@ -4,12 +4,15 @@
 #
 # To get started, you will need to upload a few CSV/Excel files.
 
+# deploy: rsconnect::deployApp("Librelink")
+
+
 library(shiny)
 
 library(tidyverse)
 library(lubridate)
 library(ggthemes)
-
+library(cgmr)
 # one-time setup creates a few dataframes for glucose levels and activity
 
 libre_raw <- readxl::read_excel("Librelink.xlsx")
@@ -34,50 +37,6 @@ glucose_raw <- glucose
 
 theme_set(theme_stata())
 
-# a handy ggplot object that draws a band through the "healthy" target zones across the width of any graph:
-glucose_target_gg <-   geom_rect(aes(xmin=as.POSIXct(-Inf,  origin = "1970-01-01"),
-                                     xmax=as.POSIXct(Inf,  origin= "1970-01-01"),
-                                     ymin=100,ymax=140),
-                                 alpha = 0.01, fill = "#CCCCCC",
-                                 inherit.aes = FALSE)
-
-
-# show glucose levels between start and end times
-cgm_display <- function(start=lubridate::now()-lubridate::hours(18),
-                        end=lubridate::now(),
-                        activity_df=activity_raw,
-                        glucose_df=glucose_raw) {
-    ggplot(glucose_df ,aes(x=time,y=value)) + geom_line(size=2, color = "red")+
-        geom_point(stat = "identity", aes(x=time,y=strip), color = "blue")+
-        glucose_target_gg +
-        geom_rect(data=activity_df %>% dplyr::filter(Activity == "Sleep") %>%
-                      select(xmin = Start,xmax = End) %>% cbind(ymin = -Inf, ymax = Inf),
-                  aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),
-                  fill="red",
-                  alpha=0.2,
-                  inherit.aes = FALSE) +
-        geom_rect(data=activity_raw %>% dplyr::filter(Activity == "Exercise") %>%
-                      select(xmin = Start,xmax = End),
-                  aes(xmin=xmin,xmax=xmax,ymin=-Inf,ymax=Inf),
-                  fill="blue",
-                  alpha=0.2,
-                  inherit.aes = FALSE) +
-        geom_vline(xintercept = activity_df %>%
-                       dplyr::filter(Activity == "Event" & Comment == "awake") %>% select("Start") %>% unlist(),
-                   color = "green") +
-        geom_vline(xintercept = activity_df %>%
-                       dplyr::filter(Activity == "Food") %>% select("Start") %>% unlist(),
-                   color = "yellow")+
-        geom_text(data = activity_df %>%
-                      dplyr::filter(Activity == "Food") %>% select("Start","Comment") ,
-                  aes(x=Start,y=50, angle=90, hjust = FALSE,  label = Comment),
-                  size = 6) +
-        labs(title = "Glucose (mg/dL)", subtitle = start) +  theme(plot.title = element_text(size=22))+
-        scale_x_datetime(limits = c(start,end))
-
-}
-
-
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -95,7 +54,7 @@ shinyServer(function(input, output) {
         #             activity_raw,
         #             glucose_raw)
 
-        cgm_display(lubridate::as_datetime(input$date1),
+        cgmr::cgm_display(lubridate::as_datetime(input$date1),
                     lubridate::as_datetime(input$date2),activity_raw,glucose_raw)
 
     })
